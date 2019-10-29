786a74f02db47dc530a4bb029ac7ece92b48af8f
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-29 23:41:56 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,20 @@
+public class Test {
 
+	public static void main(String args[]) {
+		
+		Runtime singletonRuntime = Runtime.getRuntime();
+
+		singletonRuntime.gc();
+		
+		System.out.println(singletonRuntime);
+		
+		Runtime anotherInstance = Runtime.getRuntime();
+		
+		System.out.println(anotherInstance);
+		
+		if(singletonRuntime == anotherInstance) {
+			System.out.println("They are the same instance");
+		}
+		
+	}
+}
