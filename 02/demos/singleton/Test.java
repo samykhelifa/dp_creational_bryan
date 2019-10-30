1f152134faf27caae246cdcf8bea3bacadc34f95
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-30 12:03:12 +0100

    Update Test.java

--- 02/demos/singleton/Test.java
+++ 02/demos/singleton/Test.java
@@ -18,3 +18,19 @@ public class Test {
 		
 	}
 }
+class DbSingleton {
+
+	private static DbSingleton instance = null;
+	private DbSingleton() {}
+	public static DbSingleton getInstance() {
+		if(instance == null) {
+			synchronized(DbSingleton.class) {
+				if(instance == null) {
+					instance = new DbSingleton();
+				}
+			}
+		}
+		
+		return instance;
+	}
+	
