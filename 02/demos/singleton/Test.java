87d75c74657d308f935e4ccf5be7cac52b05e1e6
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-30 12:24:31 +0100

    Update Test.java

--- 02/demos/singleton/Test.java
+++ 02/demos/singleton/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
 	public static void main(String args[]) {
 		Singleton instance = Singleton.getInstance();
-                System.out.println(anotherInstance);
+                System.out.println(instance);
 		Singleton anotherInstance = Singleton.getInstance();
 		if(instance == anotherInstance) {
 			System.out.println("They are the same instance");
